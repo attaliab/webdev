0e9edc7ceee5ca74e7a7713dae904592cc9c3e89
attaliab <attaliabdesign@gmail.com>
2023-05-20 11:39:48 -0400

    changed layout details

--- Weather App/script.js
+++ Weather App/script.js
@@ -4,12 +4,14 @@ const weatherDiv = document.getElementById('weather')
 
 const apiKey = '264123c8e12246e58ad144116231805'
 
+// Data
 const getData = async function(city){
     let response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=264123c8e12246e58ad144116231805&q=${city}&days=5&aqi=no&alerts=no`)
     let data = response.json()
     return data
 }
 
+// What is displayed
 const displayData = async function(city){
     let weatherData = await getData(city)
     const temperature = document.getElementById('temp')
@@ -17,16 +19,17 @@ const displayData = async function(city){
     const condition = document.getElementById('condition')
     const forecast = document.getElementById('forecast')
     console.log(weatherData)
+
     // current weather data
     let roundedTemp = Math.round(weatherData.current.temp_f)
     temperature.innerText += `${roundedTemp}°`
     location.innerText += `${weatherData.location.name}, ${weatherData.location.region}`
     condition.innerText += `${weatherData.current.condition.text}`
+
     // forecast HTML loop
-    // const 5dayArr = weatherData.forecast.forecastday
     for (let i = 0; i < 5; i++){
-        const maxTempForecast = weatherData.forecast.forecastday[i].day.maxtemp_f
-        const minTempForecast = weatherData.forecast.forecastday[i].day.mintemp_f
+        const maxTempForecast = Math.round(weatherData.forecast.forecastday[i].day.maxtemp_f)
+        const minTempForecast = Math.round(weatherData.forecast.forecastday[i].day.mintemp_f)
         const conditionForecast = weatherData.forecast.forecastday[i].day.condition.text
         const weatherIcon = weatherData.forecast.forecastday[i].day.condition.icon
        
@@ -34,11 +37,13 @@ const displayData = async function(city){
         forecastDiv.innerHTML += `<div class="row">
         <div class="col-sm-6">
           <div class="card" id='dayCard'>
-          <img src="${weatherIcon}" class="card-img-top" alt="...">
+          <img src="${weatherIcon}" class="card-img-top p-2" alt="...">
             <div class="card-body">
-              <h5 class="card-title" id='max'>${maxTempForecast}</h5>
-              <h5 class="card-title" id='min'>${minTempForecast}</h5>
-              <p class="card-text" id='condition'>${conditionForecast}</p>
+              <div class='d-flex justify-content-center justify-content-evenly pt-1'>
+                <h5 class="card-title  text-center" id='max'>${maxTempForecast}</h5>
+                <h5 class="card-title text-center" id='min' >${minTempForecast}</h5>
+              </div>
+              <p class="card-text d-flex justify-content-center text-center" id='condition'>${conditionForecast}</p>
               
             </div>
           </div>
@@ -47,7 +52,7 @@ const displayData = async function(city){
     }
 }
 
-
+// Event listener
 form.addEventListener('submit', function(event){
     //this says 'in the EVENT of a form submission, prevent default from happening'
     event.preventDefault()

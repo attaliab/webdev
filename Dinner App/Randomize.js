2e680a749ebfbf0efc314caac565ad7c1d6b9fd3
attaliab <attaliabdesign@gmail.com>
2023-06-16 00:00:23 -0400

    added new recipes and recipe js file

--- Dinner App/Randomize.js
+++ Dinner App/Randomize.js
@@ -14,13 +14,13 @@ class recipeController {
     this.recipeArray.push(recipeObj)
     }
 
-    setLocalStorage(){
-        localStorage.setItem('recipe', JSON.stringify(this.recipeArray))
-    }
-    getLocalStorage(){
-        let recipe = JSON.parse(localStorage.getItem('recipe'))
-        return recipe
-    }
+    // setLocalStorage(){
+    //     localStorage.setItem('recipe', JSON.stringify(this.recipeArray))
+    // }
+    // getLocalStorage(){
+    //     let recipe = JSON.parse(localStorage.getItem('recipe'))
+    //     return recipe
+    // }
 }
 
 
@@ -45,9 +45,15 @@ newRecipes.addRecipe(
     ['Cook soba noodles according to package. Once done, rinse the soba noodles under cool running water to cool them down and stop them from cooking, or they will turn mushy.', 'Mix honey, soy sauce, ginger, rice wine vinegar, and seasme seeds and set aside.', 'Cut cucumbers into 1/4 inch x 1 1/2 inch matchsticks pieces if you don not have a julienne tool.', 'Sautee garlic in a skillet until golden, then add shrimp.', 'Lightly sautee cucumbers and spinach.', 'In a bowl, put noodles, sauteed veggies, and shrimp topped with the sauce you set aside. Add cilantro and scallions and serve.' ]
 )
 
-// access the keys in newRecipes.recipeArray so that it can be displayed in the innerHTML div
+newRecipes.addRecipe(
+    'Dinner App/images/one-pot-creamed.jpeg',
+    'One-Pot Creamed Chicken Spinach',
+    ['Chicken thighs', 'Italian Seasoning or  Garlic powder, Smoked paprika,  Salt, and Pepper', 'Olive oil', 'Shallots', 'Garlic', 'White wine or Chicken broth', 'Heavy cream', 'Spinach'],
+    ['Pat chicken dry. Combine herbs, garlic powder, smoked paprika, salt, and pepper together in small bowl. Rub mixture on chicken thighs.', 'Heat olive oil in a large nonstick skillet set over medium heat. Add chicken to the heated oil; cook for 6 to 7 minutes per side, or until chicken is cooked through.', 'Remove chicken thighs from the pan and set aside. Set pan back over medium heat and stir in the sliced shallots; cook for a minute, or until tender. Stir in garlic and cook for 20 seconds, or until fragrant.', 'Add chicken broth or wine; whisk in sour cream and bring mixture to a boil. Make sure to whisk so that the sour cream doesn’t curdle. Whisk in heavy cream; whisk until combined.',  'Cook and stir for 2 to 3 minutes, or until the sauce is slightly thickened. Add in the spinach and cook for a minute, or until spinach is wilted. If sauce is too thick, add a little liquid (chicken broth or wine) at a time, or until you reach desired consistency.']
+)
+// access the keys in newRecipes.recipeArray so that it can be displayed in the innerHTML div 
 
-const recipes = newRecipes.recipeArray;
+
 
 
     const sunday = document.getElementById('sunday');
@@ -58,34 +64,64 @@ const recipes = newRecipes.recipeArray;
     const friday = document.getElementById('friday');
     const saturday = document.getElementById('saturday');
 
-    const daysOfTheWeek = [sunday, monday, tuesday, wednesday, thursday, friday, saturday]
+    const recipes = newRecipes.recipeArray;
+
+    const daysOfTheWeek = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
   
-const chooseRecipe = function(){
-    const randomNum = Math.floor(Math.random() * recipes.length);
-    const randomRecipe = recipes[randomNum];
-    return randomRecipe;
+
+
+
+    const findDuplicates = function(randomNumArray) {
+        return randomNumArray.filter(function(item, index) {
+            return randomNumArray.indexOf(item) !==  index;
+        })
+        
+    }
+    
+    
+    const weeklyMeals = [];
+    
+        //Creates empty list to store the random recipes
+        const chooseRecipe = function(days){
+            for(i = 0; i < days.length; i++){
+        //Generates a random number
+            const randomNum = Math.floor(Math.random() * recipes.length);
+        //Applies the random number to the recipes list index
+            const randomRecipe = recipes[randomNum];
+            weeklyMeals.push(randomRecipe);
+    }
+    findDuplicates(weeklyMeals)
+    // return weeklyMeals;
 }
+console.log(chooseRecipe(daysOfTheWeek));
+const duplicates = findDuplicates(weeklyMeals);
+console.log(duplicates);
 
-console.log(chooseRecipe());
+// Create a function that will take the list and check for duplicates and replace them
 
-    const displayData = function(days){
-        for(i = 0; i < days.length; i++){
-            days[i].innerHTML += `<div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-            <div class="col-md-4">
-                <img src="..." class="img-fluid rounded-start" alt="...">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-            </div>
-            </div>
-            </div>`;        
-        }
-    };
-displayData(daysOfTheWeek);
+
+
+
+
+
+    // const displayData = function(days){
+    //     for(i = 0; i < days.length; i++){
+    //         days[i].innerHTML += `<div class="card mb-3" style="max-width: 540px;">
+    //         <div class="row g-0">
+    //         <div class="col-md-4">
+    //             <img src="..." class="img-fluid rounded-start" alt="...">
+    //         </div>
+    //         <div class="col-md-8">
+    //             <div class="card-body">
+    //             <h5 class="card-title">Card title</h5>
+    //             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    //             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    //             </div>
+    //         </div>
+    //         </div>
+    //         </div>`;        
+    //     }
+    // };
+// displayData(daysOfTheWeek);
 
 
